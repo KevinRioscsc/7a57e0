a34f1b522c380c2d8582f3d1cccccabc8c998968
Kevin Rios <rioskevin89@gmail.com>
2022-04-08 14:34:07 -0700

    issue

--- server/db/db.js
+++ server/db/db.js
@@ -1,15 +1,14 @@
 const Sequelize = require("sequelize");
 
-
-const db = new Sequelize('messenger', 'postgres', {
+/*
+const db = new Sequelize('messenger', 'postgres', 'Kevthebest12@', {
   host: 'localhost',
-  dialect: 'postgres'/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql'*/ 
-});
-
-
-/* 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+  dialect: 'postgres'/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' 
 });*/
+
+
+const db = new Sequelize(process.env.DATABASE_URL || "localhost", {
+  logging: false
+});
 
 module.exports = db;
