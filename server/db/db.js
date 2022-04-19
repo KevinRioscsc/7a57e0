f7962d26bc72c81ef280094b6d2d4bea2bec9dbc
Kevin Rios <rioskevin89@gmail.com>
2022-04-18 17:10:14 -0700

    finalChanges

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,8 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize('messenger', 'postgres', 'Kevthebest12@', {
-  host: 'localhost',
-  dialect: 'postgres'/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
-});
 
-/*
 const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
-});*/
+});
 
 module.exports = db;
